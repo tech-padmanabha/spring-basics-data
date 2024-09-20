b86a89c1f3ba4094d537d215b1ca88cbc9a8fff6
padmanabha <tech.padmanabha@gmail.com>
2024-09-20 18:53:01 +0530

    cleaned setup

--- src/main/java/io/pn/controller/DepartmentController.java
+++ src/main/java/io/pn/controller/DepartmentController.java
@@ -1,34 +1,28 @@
 package io.pn.controller;
 
-import java.util.List;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.pn.dto.DepartmentDto;
-import io.pn.entity.Employee;
 import io.pn.service.DepartmentServiceImpl;
 
 @RestController
+@RequestMapping("/dept")
 public class DepartmentController {
 	
 	@Autowired
 	DepartmentServiceImpl service;
 	
-	@GetMapping("/get-dept")
+	@GetMapping("/get-all")
 	public Set<DepartmentDto> getDepartment(){
 		Set<DepartmentDto> sets = service.getDepartment();
 		
 		return sets;
 	}
 	
-	
-	@GetMapping("/get-emp")
-	public List<Employee> getEmployee(){
-		
-		return null;
-	}
 
 }
--- src/main/java/io/pn/repository/DepartmentRepository.java
+++ src/main/java/io/pn/repository/DepartmentRepository.java
@@ -1,9 +1,6 @@
 package io.pn.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import io.pn.entity.Department;
@@ -12,6 +9,5 @@ import io.pn.entity.Department;
 @Repository
 public interface DepartmentRepository extends JpaRepository<Department, Integer> {
 	
-	@Query("SELECT t FROM Department t JOIN FETCH t.employees" )
-	List<Department> readAllDeparment();
+	
 }
